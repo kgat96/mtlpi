1a9eff947cb3ae71fd7db8814a239ab4144d6bd2
Kage Shen <kkshen@ingenic.cn>
2014-10-16 11:08:03 +0800

    add socket IF test func

---
+++ sockets/ud_ucase.h
@@ -0,0 +1,14 @@
+#ifndef __UD_UCASE_H__
+#define __UD_UCASE_H__
+
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <ctype.h>
+#include "tlpi_hdr.h"
+
+#define BUF_SIZE 10
+
+#define SV_SOCK_PATH "/tmp/ucase"
+
+#endif
+
---
+++ sockets/us_xfr.h
@@ -0,0 +1,12 @@
+
+#ifndef __US_XFR_H__
+#define __US_XFR_H__
+
+#include <sys/un.h>
+#include <sys/socket.h>
+#include "tlpi_hdr.h"
+
+#define SV_SOCK_PATH "/tmp/xfr"
+#define BUF_SIZE 10
+
+#endif
