15d739400b633f1976d462f96e215dae8d6218d6
Kage Shen <kkshen@ingenic.cn>
2015-03-26 17:10:05 +0800

    add mmap test 3

--- lib/get_num.c
+++ lib/get_num.c
@@ -5,6 +5,50 @@
 #include <errno.h>
 #include "get_num.h"
 
+#include <string.h>
+#include <sys/wait.h>
+#include "tlpi_hdr.h"
+
+/* NOTE: The following function employs printf(), which is not
+   async-signal-safe (see Section 21.1.2). As such, this function is
+   also not async-signal-safe (i.e., beware of calling it from a
+   SIGCHLD handler). */
+
+void                    /* Examine a wait() status using the W* macros */
+printWaitStatus(const char *msg, int status)
+{
+    if (msg != NULL)
+        printf("%s", msg);
+
+    if (WIFEXITED(status)) {
+        printf("child exited, status=%d\n", WEXITSTATUS(status));
+
+    } else if (WIFSIGNALED(status)) {
+        printf("child killed by signal %d (%s)",
+                WTERMSIG(status), strsignal(WTERMSIG(status)));
+#ifdef WCOREDUMP        /* Not in SUSv3, may be absent on some systems */
+        if (WCOREDUMP(status))
+            printf(" (core dumped)");
+#endif
+        printf("\n");
+
+    } else if (WIFSTOPPED(status)) {
+        printf("child stopped by signal %d (%s)\n",
+                WSTOPSIG(status), strsignal(WSTOPSIG(status)));
+
+#ifdef WIFCONTINUED     /* SUSv3 has this, but older Linux versions and
+                           some other UNIX implementations don't */
+    } else if (WIFCONTINUED(status)) {
+        printf("child continued\n");
+#endif
+
+    } else {            /* Should never happen */
+        printf("what happened to this child? (status=%x)\n",
+                (unsigned int) status);
+    }
+}
+
+
 /* Print a diagnostic message that contains a function name ('fname'),
    the value of a command-line argument ('arg'), the name of that
    command-line argument ('name'), and a diagnostic error message ('msg'). */
--- threads/simple_thread.c
+++ threads/simple_thread.c
@@ -27,7 +27,3 @@ int main(int argc, char *argv[])
 
 	exit(EXIT_SUCCESS);
 }
-
-
-
-
